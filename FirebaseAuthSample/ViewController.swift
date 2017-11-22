47c80ef9a7f4eb59a9bca5042f15e624f9ee8c00
Masahiko Sato <sato_m@mediaxis.jp>
2017-11-22 19:09:56 +0900

    implement GoogleSignIn

--- FirebaseAuthSample/ViewController.swift
+++ FirebaseAuthSample/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import FirebaseAuth
+import GoogleSignIn
 
 class ViewController: UIViewController {
+    @IBOutlet weak var googleSignInButton: GIDSignInButton!
+    private var handler: FIRAuthStateDidChangeListenerHandle?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().signIn()
     }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let user = FIRAuth.auth()?.currentUser
+        print(user)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//        handler = FIRAuth.auth()?.addStateDidChangeListener { (auth, user) in
+//            if let user = user {
+//                print("User is signed in with uid:", user.uid)
+//            } else {
+//                print("No user is signed in.")
+//            }
+//        }
     }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+//        if let handler = handler {
+//            FIRAuth.auth()?.removeStateDidChangeListener(handler)
+//        }
+    }
+}
 
-
+extension ViewController: GIDSignInUIDelegate {
+    
 }
 
